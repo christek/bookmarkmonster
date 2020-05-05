2c593fb7e015109cf430f588400e27873cba9980
Christopher Tokar <christopher.tokar@ibm.com>
2020-05-05 13:57:01 +0200

    removing unused code

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,11 +1,8 @@
 // @flow
 
-import { createSelector } from 'reselect';
-
 import type { State } from '../types';
 
 // TODO: do we really need seperate selectors?
 
 export const todosSelector = (state: State) => state.todos;
 export const tagsSelector = (state: State) => state.tags;
-const visibilityFilterSelector = (state: State) => state.visibilityFilter;
